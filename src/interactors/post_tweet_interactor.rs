b7a56bbd1ad2f13a8ac6790c3465b4845e132f20
yongikim <yongi.kim08@gmail.com>
2020-07-20 13:40:10 +0900

    Spawn child process to edit tweet text in vim

--- src/interactors/post_tweet_interactor.rs
+++ src/interactors/post_tweet_interactor.rs
@@ -1,17 +1,31 @@
 use crate::apis;
 
-use std::io::{Stdout, Write};
+use std::error::Error;
+use std::io::prelude::*;
+use std::io::{stdout, Stdout, Write};
+use std::os::unix::process::CommandExt;
+use std::process::{Command, Stdio};
 use termion::raw::RawTerminal;
-use termion::screen::AlternateScreen;
+use termion::screen::{AlternateScreen, ToMainScreen};
 use termion::{clear, cursor};
 
 pub fn call(screen: &mut AlternateScreen<RawTerminal<Stdout>>) {
-    let credits = apis::credits::get_credits();
-    let text = "debug".to_string();
-    let resp = apis::tweets::post_tweet(&credits, &text);
+    /*
+     * TODO: vi起動用のサービス？を作り、パターンマッチで処理する。
+     *       そのサービスでは、File I/Oも担う。
+     */
+    let mut process = match Command::new("vi").spawn() {
+        Err(why) => panic!("{}", why),
+        Ok(process) => process,
+    };
+    let result = process.wait().unwrap();
 
-    // for debug
+    // let credits = apis::credits::get_credits();
+    // let text = "debug3".to_string();
+    // let resp = apis::tweets::post_tweet(&credits, &text);
+
+    /* for debug */
     write!(screen, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
-    write!(screen, "{:#?}", resp).unwrap();
+    write!(screen, "{:?}", result).unwrap();
     screen.flush().unwrap();
 }
