a619432fb373c11abd8c1af97d6e93fdfdd1f4a9
yongikim <yongi.kim08@gmail.com>
2020-08-11 14:51:05 +0900

    Implement tweet composer function

--- src/interactors/post_tweet_interactor.rs
+++ src/interactors/post_tweet_interactor.rs
@@ -1,31 +1,40 @@
 use crate::apis;
 
-use std::error::Error;
-use std::io::prelude::*;
-use std::io::{stdout, Stdout, Write};
-use std::os::unix::process::CommandExt;
-use std::process::{Command, Stdio};
-use termion::raw::RawTerminal;
-use termion::screen::{AlternateScreen, ToMainScreen};
+use std::io::{stdin, Write};
+use termion::event::*;
+use termion::input::TermRead;
 use termion::{clear, cursor};
 
-pub fn call(screen: &mut AlternateScreen<RawTerminal<Stdout>>) {
-    /*
-     * TODO: vi起動用のサービス？を作り、パターンマッチで処理する。
-     *       そのサービスでは、File I/Oも担う。
-     */
-    let mut process = match Command::new("vi").spawn() {
-        Err(why) => panic!("{}", why),
-        Ok(process) => process,
-    };
-    let result = process.wait().unwrap();
-
-    // let credits = apis::credits::get_credits();
-    // let text = "debug3".to_string();
-    // let resp = apis::tweets::post_tweet(&credits, &text);
+pub fn call<W: Write>(writer: &mut W) {
+    let text = compose_tweet(writer);
+    let credits = apis::credits::get_credits();
+    let resp = apis::tweets::post_tweet(&credits, &text);
 
     /* for debug */
-    write!(screen, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
-    write!(screen, "{:?}", result).unwrap();
-    screen.flush().unwrap();
+    write!(writer, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
+    write!(writer, "{:?}", resp).unwrap();
+    writer.flush().unwrap();
+}
+
+fn compose_tweet<W: Write>(writer: &mut W) -> String {
+    write!(writer, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
+    writer.flush().unwrap();
+
+    let stdin = stdin();
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Ctrl('c') => {
+                break;
+            }
+            Key::Char(c) => {
+                /* TODO: store and edit text */
+                write!(writer, "{}", c).unwrap();
+            }
+            _ => {}
+        }
+
+        writer.flush().unwrap();
+    }
+
+    "hi everyone.".to_string()
 }
